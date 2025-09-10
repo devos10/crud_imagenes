72316f5f78e4ce9355648780d0ff9b8f48b24f66
devos10 <osbaldo081004@gmail.com>
2025-09-10 12:34:39 -0600

    Agregando formulario al modal con estilos

--- index.php
+++ index.php
@@ -23,11 +23,16 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Nuevo Registro</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        ...
+                        <form action="" enctype="multipart/form-data">
+                            <input type="file" class="form-control" name="imagen">
+                            <input type="submit" value="Registrar" name="btnRegistrar" class="form-control btn btn-success">
+
+
+                        </form>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
