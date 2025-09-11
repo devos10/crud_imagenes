823699cd49a57ac3dc5af6f018d1a4817c22a22c
devos10 <osbaldo081004@gmail.com>
2025-09-11 13:56:03 -0600

    arreglando errores de set_chartser y de date_default_timezone_set

--- model/conexion.php
+++ model/conexion.php
@@ -1,4 +1,4 @@
 <?php
 $conexion= new mysqli("localhost","root","100408ovc","crud_imagenes");
-$conexion->set_charset("uft8");
-date_default_timezone_set("America/México");
+$conexion->set_charset("utf8");
+date_default_timezone_set("America/Mexico_City");
