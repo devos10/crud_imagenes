ac7677948b86eb1402fad5f7ffc1205b70058f53
devos10 <osbaldo081004@gmail.com>
2025-09-12 07:27:09 -0600

    agregando de nuevo

--- index.php
+++ index.php
@@ -33,6 +33,10 @@
 
 
                         </form>
+                    </div>
+        <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
                     </div>            
                 </div>
             </div>
