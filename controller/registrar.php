07bbae78c4c1a71fab576f69736845adb1408cf7
devos10 <osbaldo081004@gmail.com>
2025-09-12 08:56:30 -0600

    creamos un array con los formatos permitidos, despues hacemos una validacion para ver si la extension de la imagen se encuentra dentro de los formatos permitidos

--- controller/registrar.php
+++ controller/registrar.php
@@ -3,7 +3,15 @@
         $imagen=$_FILES['imagen']['tmp_name']; //php asigna la ruta y el nombre de un archivo temporal generado en el servidor al subir un archivo a través de un formulario HTML.
         $nombre_imagen=$_FILES['imagen']['name']; //aqui obtenemos el nombre del archivo original
         $tipo_imagen=strtolower(pathinfo($nombre_imagen,PATHINFO_EXTENSION)); //CON ESTA VARIABLE OBTENEMOS LA EXTENSION DE NUESTRA IMAGEN original
-        echo "<div class='alert alert-info'>$tipo_imagen</div>";
+        var_dump($tipo_imagen);
+        //verificando la extension que solo sea de una imagen o foto 
+        //creamos un array con los formato permitidos
+        $formatos_permitidos=['jpg','jpeg','png','webp','svg','gif'];
+        if(in_array($tipo_imagen,$formatos_permitidos)){
+
+        }else{
+                echo "<div class='alert alert-info'>FORMATO no permitido</div>";
+        }
 
     }
 ?>
