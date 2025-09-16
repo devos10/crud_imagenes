88de3681b94a910095201bd9993be05763c5a4ad
devos10 <osbaldo081004@gmail.com>
2025-09-16 10:36:08 -0600

    Este archivo contiene toda la logica para eliminar la imagen y que se borre directamente de la bd

---
+++ controller/eliminar.php
@@ -0,0 +1,31 @@
+<?php
+
+    if(!empty($_GET['id']) && !empty($_GET['nombre'])){
+        $id=$_GET['id'];
+        $nombre=$_GET['nombre'];
+
+        try{
+                unlink($nombre);
+
+            }catch(\Throwable $th){
+
+            }
+        //eliminamos en la base de datos
+        $eliminar=$conexion->query("DELETE FROM imagenes WHERE id_imagen=$id");
+
+        if($eliminar==1){
+            echo "<div class='alert alert-success'>La imagen fue ELIMINADA con éxito</div>";
+
+        }else{
+            echo "<div class='alert alert-danger'>ERROR al eliminar la imagen</div>";
+
+
+        }
+
+    }
+
+?>
+      <script>
+        //con este script lo que hacemos es que no salga la alerta de reenvio de formulario cuando recargamos la pagina
+        history.replaceState(null,"",location.href);
+    </script>
